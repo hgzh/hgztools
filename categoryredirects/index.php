89a2d86a6b2b92e7f6fa642379c4fab6a9268f98
hgzh <konradfl@outlook.com>
2015-09-27 13:44:54 +0200

    add

---
+++ categoryredirects/index.php
@@ -0,0 +1,91 @@
+<?php
+	/**
+	 * This program is free software; you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as published by
+	 * the Free Software Foundation; either version 2 of the License, or
+	 * (at your option) any later version.
+	 *
+	 * This program is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	 * GNU General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU General Public License
+	 * along with this program; if not, write to the Free Software
+	 * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+	 */
+	
+	error_reporting(E_ALL);
+	require_once('/data/project/hgztools/public_html/general.php');
+	
+	// create new page object
+	$page = new HtmlPage('Category redirects');
+
+	// create new database object
+	$db = new Database();
+	
+	// get parameters from url
+	$par_lang    = (isset( $_GET['lang'])    && $_GET['lang']    != '' ) ? strtolower( $_GET['lang'] )    : '';
+	$par_project = (isset( $_GET['project']) && $_GET['project'] != '' ) ? strtolower( $_GET['project'] ) : '';
+	
+	$page->addInline('p', 'This tool shows a list of categories that redirect to another category in the given project and the number of entries in it.');
+	$page->addInline('h2', 'Options');
+	
+	$optionForm = new HtmlForm('index.php', 'GET');
+	$optionForm->addHTML('<table class="iw-nostyle">');
+	
+	$optionForm->addHTML('<tr><td>');
+	$optionForm->addLabel('lang', 'Language');
+	$optionForm->addHTML('</td><td>');
+	$optionForm->addInput('lang', $par_lang, 'Language code of the project, e.g. de', 7, true);
+	$optionForm->addHTML('</td></tr>');
+	
+	$optionForm->addHTML('<tr><td>');
+	$optionForm->addLabel('project', 'Project');
+	$optionForm->addHTML('</td><td>');
+	$optionForm->addInput('project', $par_project, 'Project code, e.g wikipedia', 20, true);
+	$optionForm->addHTML('</td></tr>');
+	
+	$optionForm->addHTML('<tr><td colspan="2">');
+	$optionForm->addButton('submit', 'View categories');
+	$optionForm->addHTML('</td></tr>');
+	
+	$optionForm->addHTML('</table>');
+	$optionForm->output();
+	
+		
+	if (isset($par_lang) && $par_lang != '' & isset($par_project) && $par_project != '') {
+		
+		if (!preg_match( '/^[a-z]{1,7}$/', $par_lang) || !preg_match('/^[a-z]{1,15}$/', $par_project) ) {
+			$page->setMessage('Please enter  valid language and project codes.', true);
+		}
+		
+		$page->addInline('h2', 'Results');
+		
+		$db->replicaConnect(Database::getName($par_lang, $par_project));
+		$t1  = 'SELECT page.page_title, count(categorylinks.cl_from) AS cl ';
+		$t1 .= 'FROM page LEFT JOIN categorylinks ON page.page_title = categorylinks.cl_to ';
+		$t1 .= 'WHERE page.page_namespace = 14 AND page.page_is_redirect = 1 GROUP BY page.page_title ORDER BY page.page_title;';
+		$q1 = $db->query($t1);
+		
+		if ($db->num_rows === 0) {
+			$page->addInline('p', 'there were no results for this query', 'iw-info');
+		} else {
+			$page->openBlock('table', 'iw-table');
+			$page->addInline('tr', '<th>Name</th><th>Entries</th>');
+			while ($l1 = $q1->fetch_assoc()) {
+				$page->openBlock('tr');
+				$page->addInline('td', '<a href="http://' . $par_lang . '.' . $par_project . '.org/wiki/Category:' . $l1['page_title'] . '?redirect=no">' . $l1['page_title'] . '</a>');
+				$page->addInline('td', $l1['cl']);
+				$page->closeBlock();
+			}
+			$page->closeBlock();
+		}
+		
+		$q1->close();
+	
+	}
+	
+	$page->output();
+	$db->close();
+?>
---
+++ general.php
@@ -0,0 +1,422 @@
+<?php
+	/**
+	 * ##### general.php #####
+	 * Allgemeine Klassen und Methoden
+	 *
+	 */
+	error_reporting(E_ALL);
+		
+	/**
+	 * ##### CLASS HtmlPage CLASS #####
+	 * Klasse für Seitenaufbau
+	 */
+	class HtmlPage {
+		
+		/**
+		 * [string]
+		 * angezeigter Seiteninhalt
+		 */
+		protected static $content = '';
+		
+		/**
+		 * [string]
+		 * Auszeichnungen im Kopfbereich
+		 */
+		private $head = '';
+		
+		/**
+		 * [string]
+		 * Definition der Navigationsleiste
+		 */
+		private $navigation = '';
+		
+		/**
+		 * [string]
+		 * Systemnachricht(en)
+		 */
+		private $message = '';
+		
+		/**
+		 * [string]
+		 * Auszeichnungen im Fußbereich
+		 */
+		private $foot = '';
+
+		/**
+		 * [SplStack]
+		 * Stack, der zum Schließen geöffneter HTML-Tags benötigt wird
+		 */		
+		private $stack;
+		
+        /**
+         * Klassenkonstruktor
+		 * Initialisiert die Ausgabevariablen mit Standardtext
+		 *
+		 * @parameter
+		 * - title : optionaler Seitentitel
+		 *
+		 */
+		public function __construct($title = '') {			
+			// gesamten head-Bereich innerhalb des <html>-Tags setzen
+			$this->head .= '<!DOCTYPE html>';
+			$this->head .= '<html>';
+			$this->head .= '<head>';
+			$this->head .= '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8">';
+			$this->head .= '<link rel="stylesheet" type="text/css" href="main.css">';
+			$this->head .= '<title>' . $title . '</title>';
+			$this->head .= '</head>';
+			$this->head .= '<body>';
+			$this->head .= '<div class="iw-wrapper">';
+			$this->head .= '<div class="iw-headline">' . $title . '</div>';
+			
+			// footer setzen, Dokument beenden
+			$this->foot .= '</div><div class="iw-footer">';
+			$this->foot .= 'Powered by <a href="http://wikitech.wikimedia.org">Wikimedia Labs</a>.';
+			$this->foot .= '</div></div>';
+			$this->foot .= '</body>';
+			$this->foot .= '</html>';
+			
+			$this->stack = new \SplStack;
+		}
+		
+		/**
+		 * erstellt die Navigationsleiste.
+		 *
+		 * @parameter
+		 * - navInfo    : assoziatives Array der Form 'Anzeigetext' => 'URL'
+		 *
+		 */
+		public function setNavigation(array $navInfo) {			
+			$k1 = ''; // [string] Key 1
+			$v1 = ''; // [string] Value 1
+			
+			$this->navigation  = '<div class="iw-navigation">';
+			$this->navigation .= '<ul class="iw-navigation">';
+			foreach ($navInfo as $k1 => $v1) {
+				$this->navigation .= '<li><a href="' . $v1 . '" title="' . $k1 . '">' . $k1 . '</a></li>';
+			}
+			$this->navigation .= '</ul>';
+			$this->navigation .= '</div>';
+		}
+		
+		/**
+		 * setzt den benutzerdefinierten Inhaltsbereich
+		 *
+		 * @parameter
+		 * - content : Seiteninhalt
+		 */
+		public function setContent($content) {
+			self::$content .= $content;
+		}
+		
+		/**
+		 * setzt eine Systemnachricht an die dafür vorgesehene Position mit entsprechendem Styling.
+		 * 
+		 * @parameter
+		 * - id       : Kennung der Systemnachricht
+		 * - endAfter : Seite nach der Nachricht beenden
+		 */
+		public function setMessage($message, $endAfter = false) {
+			$this->message .= '<div class="iw-info">';
+			$this->message .= $message;
+			$this->message .= '</div>';
+			
+			if ($endAfter == true) {
+				$this->output();
+				die;
+			}
+		}
+		
+		/**
+		 * fügt ein HTML-Element mit der Möglichkeit ein, weitere darinliegende Elemente zu definieren.
+		 *
+		 * @parameter
+		 * - tag   : Name des Tags
+		 * - class : Klassenangaben
+		 * - style : CSS-Style-Angaben
+		 */
+		public function openBlock($tag, $class = '', $style = '') {
+			self::$content .= '<' . $tag;
+			if (isset($class) && $class != '') {
+				self::$content .= ' class="' . $class . '"';
+			}
+			if (isset($style) && $style != '') {
+				self::$content .= ' style="' . $style . '"';
+			}
+			self::$content .= '>';
+			$this->stack->push($tag);
+		}
+
+		/**
+		 * beendet ein mit openBlock() geöffnetes Element. Diese Funktion benutzt einen Stack, das zuletzt
+		 * geöffnete Tag wird als erstes wieder geschlossen.
+		 *
+		 * @parameter
+		 * - nr : Anzahl der mit einem Aufruf zu schließenden Tags
+		 */
+		public function closeBlock($nr = 1) {
+			$tag = ''; // [string] Tag aus dem Stack
+			$i   = ''; // [value] Hochzählen
+			
+			for ($i = 0; $i < $nr; $i++) {
+				$tag = $this->stack->pop();
+				self::$content .= '</' . $tag . '>';
+			}
+		}
+
+		/**
+		 * fügt ein HTML-Element ohne Möglichkeit weiter Definition ein.
+		 *
+		 * @parameter
+		 * - tag     : Name des Tags
+		 * - content : Inhalt des Tags
+		 * - class   : Klassenangaben
+		 * - style   : CSS-Style-Angaben
+		 */		
+		public function addInline($tag, $content, $class = '', $style = '') {
+			self::$content .= '<' . $tag;
+			if (isset($class) && $class != '') {
+				self::$content .= ' class="' . $class . '"';
+			}
+			if (isset($style) && $style != '') {
+				self::$content .= ' style="' . $style . '"';
+			}
+			self::$content .= '>';
+			self::$content .= $content;
+			self::$content .= '</' . $tag . '>';
+		}
+		
+		/**
+		 * fügt beliebigen HTML-Code an der aktuellen Position ein
+		 *
+		 * @parameter
+		 * - code : einzufügender Code
+		 */
+		public function addHTML($code) {
+			self::$content .= $code;
+		}
+		
+		/**
+		 * gibt die komplette HTML-Seite aus
+		 */
+		public function output() {
+			echo $this->head;
+			echo $this->navigation;
+			echo '<div class="iw-content">';
+			// echo $this->message;
+			echo self::$content;
+			echo $this->foot;
+		}
+		
+	}
+	
+	/**
+	 * ##### CLASS HtmlForm CLASS #####
+	 * Klasse für HTML-Form-Elemente
+	 *
+	 * @erweitert
+	 * - HtmlPage
+	 */
+	class HtmlForm extends HtmlPage {
+		
+		/**
+		 * [string]
+		 * Inhalt des HtmlForms
+		 */
+		private $form = '';
+		
+		/**
+		 * Klassenkonstruktor
+		 *
+		 * @parameter
+		 * - action : Zielseite und -parameter
+		 * - method : HTTP-Request-Methode
+		 */
+		public function __construct($action, $method) {
+			$this->form = '<form action="' . $action . '" method="' . $method . '">';
+		}
+		
+		/**
+		 * fügt ein Beschriftungsfeld hinzu
+		 *
+		 * @parameter
+		 * - for  : Name des beschrifteten Feldes
+		 * - text : Beschriftungstext
+		 */
+		public function addLabel($for, $text) {
+			$this->form .= '<label for="' . $for . '">' . $text . ': </label>';
+		}
+		
+		/**
+		 * fügt ein Optionselement hinzu
+		 *
+		 * @parameter
+		 * - name  : Bezeichner
+		 * - value : Wert des Elements
+		 * - text  : Beschriftung
+		 * - corr  : korrespondierende Variable
+		 * - break : Umbruch nach Element
+		 */
+		public function addRadio($name, $value, $text, $corr, $break = true) {
+			$this->form .= '<input type="radio" ';
+			$this->form .= 'name="' . $name . '" value="' . $value . '"';
+			if( $value == $corr ) {
+				$this->form .= ' checked="checked" ';
+			}
+			$this->form .= '/> ' . $text;
+			if( $break == true ) {
+				$this->form .= '<br />';
+			}
+		}
+		
+		/**
+		 * fügt ein Auswahlelement hinzu
+		 *
+		 * @parameter
+		 * - name  : Bezeichner
+		 * - value : Wert des Elements
+		 * - text  : Beschriftung
+		 * - corr  : korrespondierende Variable
+		 * - break : Umbruch nach Element
+		 */
+		public function addCheckbox($name, $value, $text, $corr, $break = true) {
+			$this->form .= '<input type="checkbox" ';
+			$this->form .= 'name="' . $name . '" value="' . $value . '"';
+			if( $value == $corr ) {
+				$this->form .= ' checked="checked" ';
+			}
+			$this->form .= '/> ' . $text;
+			if( $break == true ) {
+				$this->form .= '<br />';
+			}
+		}
+		
+		/**
+		 * fügt ein Eingabefeld (String) hinzu
+		 *
+		 * @parameter
+		 * - name      : Bezeichner
+		 * - value     : Voreingetragener Text
+		 * - info      : zusätzliche Beschriftung
+		 * - maxlength : maximal erlaubte Zeichenanzahl bei Eingabe
+		 * - required  : benötigt, um Formular absenden zu können
+		 * - type      : Typ des Eingabefelds (z.B. text, password)
+		 */
+		public function addInput($name, $value, $info = '', $maxlength = 0, $required = false, $type='text') {
+			$this->form .= '<input type="' . $type . '" ';
+			$this->form .= 'name="' . $name . '" id="' . $name . '" value="' . $value . '"';
+			if( $maxlength != 0 ) {
+				$this->form .= ' maxlenghth="' . $maxlength . '" ';
+			}
+			if( $required == true ) {
+				$this->form .= ' required="required" ';
+			}
+			$this->form .= '/>';
+			if( $info != '' ) {
+				$this->form .= ' <span class="iw-info">' . $info . '</span>';
+			}
+		}
+		
+		/**
+		 * fügt einen Button hinzu
+		 *
+		 * @parameter
+		 * - type  : Buttontyp
+		 * - value : Beschriftung
+		 */
+		public function addButton($type, $value) {
+			$this->form .= '<input type="' . $type . '" value="' . $value . '" />';
+		}
+		
+		/**
+		 * fügt an aktueller Stelle beliebigen Code ein
+		 *
+		 * @parameter
+		 * - code  : einzufügender Code
+		 */
+		public function addHTML($code) {
+			$this->form .= $code;
+		}
+		
+		/**
+		 * beendet das Formular gibt dessen kompletten HTML-Code zurück
+		 *
+		 * @parameter
+		 * - return : wenn true, wird Code in Variable zurückgegeben, ansonsten in content des HtmlPage-Objektes
+		 *
+		 * @rückgabe
+		 * - HTML-Code
+		 */
+		public function output($return = false) {
+			$this->form .= '</form>';
+			if ($return == false) {
+				parent::$content .= $this->form;
+			} else {
+				return $this->form;
+			}
+		}
+	}
+	
+	/**
+	 * ##### CLASS Database CLASS #####
+	 * Klasse für Datenbankfunktionen
+	 *
+	 * @erweitert
+	 * - mysqli
+	 */
+	class Database extends mysqli {
+		
+		/**
+		 * Verbindung mit Datenbank herstellen.
+		 *
+		 * @parameter
+		 * - database    : Name der Datenbank
+		 *
+		 */
+		public function replicaConnect($database) {
+			
+			$mycnf = parse_ini_file( "/data/project/hgztools/replica.my.cnf" );
+			$cluster = ( preg_match( '/[-_]p$/', $database ) ) ? substr( $database, 0, -2 ) : $database;
+			parent::connect($cluster . '.labsdb', $mycnf['user'], $mycnf['password']);
+			unset($mycnf);
+			
+			if( $this->connect_error ) {
+				die( '<p><strong>Database server login failed.</strong> '
+				. ' This is probably a temporary problem with the server and will be fixed soon. '
+				. ' The server returned error code ' . $this->connect_errno . '.</p>' );
+			}
+			
+			$res = $this->select_db(str_replace('-', '_', $database));
+			
+			if( $res === false ){
+				die( '<p><strong>Database selection failed.</strong> '
+				. ' This is probably a temporary problem with the server and will be fixed soon.</p>' );
+			}
+			
+		}
+
+		/**
+		 * Datenbanknamen ermitteln
+		 *
+		 * @parameter
+		 * - lang      : Sprache
+		 * - project   : Projekt
+		 * - separator : Separator
+		 *
+		 */		
+		public static function getName($lang, $project, $separator = '-') {
+			if( $project == 'wikipedia' ) {
+				$project = 'wiki';
+			}
+			if( $project == 'wikimedia' ) {
+				$project = 'wiki';
+			}
+			if( $project == 'wikidata' ) {
+				$project = 'wiki';
+				$lang = 'wikidata';
+			}
+			return $lang . $project . $separator . 'p';
+		}
+		
+	}
+?>
