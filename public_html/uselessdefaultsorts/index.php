c0f87bd8ee5a896dc9aa5b3825fb9689b0a9717e
hgzh <konradfl@outlook.com>
2015-09-29 11:16:19 +0200

    try replace

--- public_html/uselessdefaultsorts/index.php
+++ public_html/uselessdefaultsorts/index.php
@@ -72,8 +72,8 @@
 		
 		$db->replicaConnect(Database::getName($par_lang, $par_project));
 		$t1  = 'SELECT page_title, page_namespace, pp_value FROM page, page_props';
-		$t1 .= ' WHERE pp_propname = \'defaultsort\' AND pp_page = page_id AND page_title = pp_value';
-		$t1 .= ' ORDER BY pp_sortkey;';
+		$t1 .= ' WHERE pp_propname = \'defaultsort\' AND pp_page = page_id AND REPLACE(page_title, \'_\', \' \') = pp_value';
+		$t1 .= ' ORDER BY page_title;';
 		
 		$q1 = $db->query($t1);
 		if ($q1->num_rows === 0) {
